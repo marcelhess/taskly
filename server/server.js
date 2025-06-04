c29236b2b33603e67c9ceb212588daa0cab09a81
Marcel <marcel.hess.ch@outlook.com>
2025-06-04 07:39:02 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+// Create a new "/api" route
+app.use("/api", (req, res) => {
+   res.status(200).json({ message: "Hello World!" });
+});
+
+app.use("*", (req, res) => {
+   res.status(404).json({ message: "not found" });
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+   console.log(`Server listening on port ${PORT}`);
+});
