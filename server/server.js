4670071165c5391367fc4e11a07c6cb400559110
Marcel <marcel.hess.ch@outlook.com>
2025-06-05 07:15:09 +0200

    Created user API router and controller

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import { db } from "./libs/dbConnect.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
+app.use(express.json());
 const PORT = 8000;
 
-// Create a new "/api" route
-app.use("/api", (req, res) => {
-   res.status(200).json({ message: "Hello World!" });
-});
+// Create a new "/api/v1/users" route
+app.use("/api/v1/users", userRouter);
 
 app.use("*", (req, res) => {
    res.status(404).json({ message: "not found" });
