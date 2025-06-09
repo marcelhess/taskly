5033c3425b812b657129e6b35da9693575497a41
Marcel <marcel.hess.ch@outlook.com>
2025-06-09 14:49:53 +0200

    Getting started with React

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App() {
+   const [visibility, setVisibility] = useState(true);
+
+   const toggleDiv = () => {
+      setVisibility(!visibility);
+   };
+
+   return (
+      <div>
+         {visibility && (
+            <div id="myDiv">
+               <p>Hello World!</p>
+            </div>
+         )}
+         <button onClick={toggleDiv}>Toggle Div</button>
+      </div>
+   );
+}
+
+export default App;
